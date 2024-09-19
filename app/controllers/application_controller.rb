daee3d1ba9bdc643dac19d7fc1e057cc1554ac41
Rohit Dandnayak <rohit.dandnayak@soroco.com>
2024-09-19 10:24:31 +0530

    respond_to added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include ActionController::MimeResponds
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   protect_from_forgery with: :null_session
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,22 @@
 class ArticlesController < ApplicationController
+  # include ActionController::MimeResponds
+
   def index
     @articles = Article.all
     # render json: @articles
+      # respond_to :json, :html
+    respond_to do |format|
+      format.html 
+      format.json { render json: @articles }
+    end
   end
   def show
     @article = Article.find(params[:id])
     # render json: @article
+    respond_to do |format|
+      format.html 
+      format.json { render json: @article }
+    end
   end
   def new
     @article = Article.new
@@ -14,17 +25,22 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if(@article.save)
-      redirect_to @article
+      respond_to do |format|
+        format.html { redirect_to @article }
+        format.json { 
+          render json: @article, status: :created, location: @article
+        }
+      end
     else
-      render :new, status: :unprocessable_entity
+      respond_to do |format|
+        format.html {
+          render :new, status: :unprocessable_entity
+        }
+        format.json { 
+          render json: @article.errors, status: :unprocessable_entity
+        }
+      end
     end
-
-    # if @article.save
-    #   render json: @article, status: :created, location: @article
-    # else
-    #   render json: @article.errors, status: unprocessable_entity
-    # end
-
   end
   def edit
     @article = Article.find(params[:id])
@@ -32,21 +48,33 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
-      redirect_to @article
+      respond_to do |format|
+        format.html { redirect_to @article }
+        format.json { 
+          render json: @article, status: :created, location: @article
+        }
+      end
     else 
-      render :edit, status: :unprocessable_entity
+      respond_to do |format|
+        format.html {
+          render :edit, status: :unprocessable_entity
+        }
+        format.json { 
+          render json: @article.errors, status: :unprocessable_entity
+        }
+      end
     end
-    # if @article.update(article_params)
-    #   render json: @article, status: :created, location: @article
-    # else
-    #   render json: @article.errors, status: unprocessable_entity
-    # end
   end
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
 
-    redirect_to root_path, status: :see_other
+    respond_to do |format|
+      format.html {
+        redirect_to root_path, status: :see_other
+      }
+      format.json
+    end
   end
 
   private
